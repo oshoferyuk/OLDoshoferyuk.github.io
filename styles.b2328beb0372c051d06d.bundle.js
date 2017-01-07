8570719d77166a9ac9a2e28963ff36d8a1aff2f4
oshoferyuk <oshoferyuk@gmail.com>
2017-01-07 09:25:20 +0200

    new todo

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,3],{1165:function(n,o,c){n.exports=c(541)},541:function(n,o){}},[1165]);
+webpackJsonp([2,4],{1166:function(n,o,c){n.exports=c(539)},539:function(n,o){}},[1166]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
