63df69d985424a647a1e8d23d7900dfb0e14f716
oshoferyuk <oshoferyuk@gmail.com>
2017-01-08 14:47:48 +0200

    edit with dynamic component

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([2,4],{1166:function(n,o,c){n.exports=c(539)},539:function(n,o){}},[1166]);
+webpackJsonp([2,4],{1168:function(n,o,c){n.exports=c(541)},541:function(n,o){}},[1168]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
